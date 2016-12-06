386e78664f3006efe598ace576c6d5aa6a3adeff
Nicholas Evans <nick@nick-evans.com>
2016-12-06 13:23:33 -0500

    hooked up google sheets

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+(function (){
+
+var footer = document.querySelector('.footer');
+if (window.location.hash){
+
+  var hash = window.location.hash.substr(1)
+  var googleSheets = '<google-sheets key="' + hash  + '" tab-id="1" client-id="41248146944-jpcqhs9lp3t69pgvlercu8roijmoqbfm.apps.googleusercontent.com"></google-sheets>';
+  footer.innerHTML = googleSheets;
+  var sheet = document.querySelector('google-sheets');
+  sheet.addEventListener('google-sheet-data', function(e) {
+
+   console.log(this.rows); //- list of the user's spreadsheets
+   // this.tab - information on the tab that was fetched
+   // this.rows - cell row information for the tab that was fetched
+  });
+
+  sheet.addEventListener('error', function(e) {
+   // e.detail.response
+  });
+} else {
+  footer.innerHTML = 'got nothing';
+}
+
+})();
